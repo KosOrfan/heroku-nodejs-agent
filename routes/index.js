8138e84352348ffde85e0cf01dfc4e4ce014e65c
KostOrv <konst.orfan@gmail.com>
2018-10-23 22:35:41 +0300

    Comment 'firebase-admin' var.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var admin = require("firebase-admin");
-
-var serviceAccount = require("C:\\Users\\ntinos\\WebstormProjects\\test-agent\\test-agent-7482c-firebase-adminsdk-lyt3f-498973cf65.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://test-agent-7482c.firebaseio.com"
-});
+// var admin = require("firebase-admin");
+//
+// var serviceAccount = require("C:\\Users\\ntinos\\WebstormProjects\\test-agent\\test-agent-7482c-firebase-adminsdk-lyt3f-498973cf65.json");
+//
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://test-agent-7482c.firebaseio.com"
+// });
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
